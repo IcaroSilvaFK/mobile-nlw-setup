8b8e643935b181943c0543cc8596248ee5d795e7
whyy <icarovsilva1@gmail.com>
2023-01-18 21:33:28 -0300

    feat: add dayjs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
 
 import HabitLogo from '../../assets/logo.svg';
 
@@ -6,6 +8,14 @@ export function Header() {
   return (
     <View className='w-full flex-row items-center justify-between'>
       <HabitLogo />
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className='flex-row h-11 px-4 border border-violet-500 rounded-lg items-center'
+      >
+        <Feather name='plus' color={colors.violet[500]} size={20} />
+        <Text className='text-white ml-3 font-semibold text-base'>Novo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
